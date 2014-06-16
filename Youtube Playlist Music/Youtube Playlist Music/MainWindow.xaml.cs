6688b9c1ebe8e4e85f870214e2c7df5a99549f37
osin <osin@live.com>
2014-06-16 23:33:26 +0200

    Ajout du projet initial + Scanne des dossier et recupération des id tags + constitution de la base de données

---
+++ Youtube Playlist Music/Youtube Playlist Music/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+using System;
+
+namespace Youtube_Playlist_Music
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Worker_Local local_worker = new Worker_Local();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dlg = new VistaFolderBrowserDialog();
+            dlg.ShowNewFolderButton = false;
+
+            if (dlg.ShowDialog() == true)
+            {
+                if (Directory.Exists(dlg.SelectedPath))
+                {
+                    this.local_worker.rootFolder = dlg.SelectedPath;
+                    System.Collections.Generic.List<String> files = new System.Collections.Generic.List<String>();
+                    files.AddRange(Directory.GetFileSystemEntries(this.local_worker.rootFolder, "*", SearchOption.AllDirectories));
+                    this.local_worker.files = new System.Collections.Generic.List<String>();
+                    this.local_worker.files.AddRange(files.FindAll(s => s.Contains(".mp3")));
+                    this.local_worker.files.AddRange(files.FindAll(s => s.Contains(".mp4")));
+                    this.local_worker.files.AddRange(files.FindAll(s => s.Contains(".m4a")));
+                    this.local_worker.files.AddRange(files.FindAll(s => s.Contains(".flac")));
+                    this.local_worker.generateDataBase();
+                }
+                else
+                {
+                    MessageBoxResult result = MessageBox.Show("Erreur sur le dossier",
+                        "Il semblerait que le dossier choisi n'existe pas", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.local_worker.rootFolder = null;
+                    this.local_worker.files = null;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Youtube Playlist Music/Youtube Playlist Music/Worker Local.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_Playlist_Music
+{
+    class Worker_Local
+    {
+        public String rootFolder { get; set; }
+        public List<String> files { get; set; }
+        public List<Dictionary<string, string>> database { get; set; }
+
+        public bool generateDataBase()
+        {
+            if (files == null || files.Count == 0)
+            {
+                return false;
+            }
+            this.database = new List<Dictionary<string, string>>();
+            files.ForEach(delegate(String path)
+            {
+                TagLib.File file = TagLib.File.Create(path);
+                Dictionary<string, string> media = new Dictionary<string, string>();
+                string artist = file.Tag.FirstPerformer != null ? file.Tag.FirstPerformer : "";
+                string title = file.Tag.Title != null ? file.Tag.Title : "";
+                media.Add(artist, title);
+                this.database.Add(media);
+            });
+            return true;
+        }
+    }
+}
